f25e0695fb9aa5bc71d0cdac9b5981f348ec8b1f
kstoriadis <federico.gonzalezc@gmail.com>
2014-04-10 12:41:49 -0300

    Kuder
    Los estilos del kuder lsitos, falta revisar el commit de php

---
+++ js/submitform.js
@@ -0,0 +1,49 @@
+var nombre;
+var emailFromVal;
+var edadVal;
+var sexoVal;
+$(document).ready(function(){
+	$("#continuar").click(function(){
+		var hasError = false;
+		var emailReg = /^([\w-\.]+@([\w-]+\.)+[\w-]{2,4})?$/;
+		$(".error").hide();
+		
+		nombre = $("#nombre").val();
+		if(nombre == '') {
+			$("#nombre").after('<span class="error">No olvides ingresar tu nombre</span>');
+			hasError = true;
+		}
+		
+		emailFromVal = $("#emailFrom").val();
+		if(emailFromVal == '') {
+			$("#emailFrom").after('<span class="error">No olvides ingresar tu email</span>');
+			hasError = true;
+		} else if(!emailReg.test(emailFromVal)) {	
+			$("#emailFrom").after('<span class="error">Es importante que ingreses una direccion de correo v&aacute;lida para recibir los resultados</span>');
+			hasError = true;
+		}
+		var regex = /[0-9]|\./;
+		edadVal = $("#edad").val();
+		if(edadVal == '' || !regex.test(edadVal)) {
+			$("#edad").after('<span class="error">No olvides poner tu edad expresada como un n&uacute;mero!</span>');
+			hasError = true;
+		}
+		
+		sexoVal = $("#sexo").val();
+		if(sexoVal == '' || sexoVal == '0') {
+			$("#sexo").after('<span class="error">Seleccione uno por favor.</span>');
+			hasError = true;
+		}
+		
+		if(hasError == false) {
+			$("#sendEmail").hide();
+			$("#cuestionario").show();
+		}
+		return false;
+		
+	});
+	$("#finalizar").click(function(){					   				   
+				
+		
+	});						   
+});
