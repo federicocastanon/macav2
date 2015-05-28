1e3f85d873394e59a4ef30a9032123fc67099473
kstoriadis <federico.gonzalezc@gmail.com>
2015-05-28 16:20:32 -0300

    timestamp
    para que los datos guardados tengan mara de horario

--- guardar.php
+++ guardar.php
@@ -2,12 +2,12 @@
 $nombre=$_POST['nombre'];
 $resultado=$_POST['resultado'];
 // Si entramos es que todo se ha realizado correctamente
-$link=mysql_connect('fdb13.biz.nf','1781873_fede','fede0909');
-mysql_select_db("1781873_fede",$link);
+$link=mysql_connect('fdb13.biz.nf','1884227_maca','Fede0909');
+mysql_select_db("1884227_maca",$link);
 // Con esta sentencia SQL insertaremos los datos en la base de datos
 mysql_query("INSERT INTO resultados (
- `resultado`, `nombre`)
-        VALUES ('$nombre','$resultado')",$link);
+ `resultado`, `nombre`, `fecha`)
+        VALUES ('$resultado','$nombre', CURRENT_TIMESTAMP)",$link);
 // Ahora comprobaremos que todo ha ido correctamente
 $my_error=mysql_error($link);
 if(!empty($my_error)) {
